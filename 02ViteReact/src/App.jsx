d672f613ea5f98394d94e994fc2585e772ae0474
Adarsh <157718190+adarshjha305@users.noreply.github.com>
2025-01-30 00:05:10 +0530

    some changes

--- 02ViteReact/src/App.jsx
+++ 02ViteReact/src/App.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  let counter = 0;
+  // let counter = 0;
+  let [counter, setCounter] = useState(15);
   const addValue = () => {
-    counter += 1;
-    console.log(counter);
+    counter = counter + 1;
+    setCounter(counter);
+    // console.log(counter);
+  }
+  const removeValue = () => {
+    setCounter(counter - 1);
   }
   return (
     <>
       <h1>Learn React </h1>
       <h2>Counter value : {counter}</h2>
 
-      <button onClick={addValue}>Add value </button>
+      <button onClick={addValue}>Add value: {counter} </button>
       <br />
-      <button>Remove value </button>
+      <button onClick={removeValue}>Remove value: {counter} </button>
     </>
   )
 }
